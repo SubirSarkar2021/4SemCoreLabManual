c0d61331d5a815ab2cef32103434326ed234fa37
Dr. Subir Sarkar <dr.subirsarkar46@gmail.com>
2025-04-11 10:05:37 +0530

    adding contents.

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '4th Sem Lab Computational Physics'
+copyright = '2025, Dr. Subir Sarkar'
+author = 'Dr. Subir Sarkar'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'nbsphinx',
+    'sphinx_rtd_theme',  # optional
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' 
+html_static_path = ['_static']
+
+
+# Optional: suppress warnings about Jupyter kernel metadata
+nbsphinx_allow_errors = True
---
+++ docs/source/index.rst
@@ -0,0 +1,28 @@
+.. 4th Sem Lab Computational Physics documentation master file, created by
+   sphinx-quickstart on Fri Apr 11 09:24:00 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+4th Sem Lab Computational Physics documentation
+===============================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   LabManual4thSemCoreSphinx/A#CoupledSpringMass
+   LabManual4thSemCoreSphinx/B#ElectricalCircuitEquation
+   LabManual4thSemCoreSphinx/C#nthRoot
+   LabManual4thSemCoreSphinx/D#Transcendental
+   LabManual4thSemCoreSphinx/E#SinkingDepth
+   LabManual4thSemCoreSphinx/F#NumericalIntegration
+   LabManual4thSemCoreSphinx/G#FourierSeriesSawToothWave
+   LabManual4thSemCoreSphinx/H#FourierSeriesSquareWave
+   LabManual4thSemCoreSphinx/I#DiracDeltaFunction
+   LabManual4thSemCoreSphinx/J#LegendrePoly
+
